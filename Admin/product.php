135e856b4dae8f18d4c21ab9e089d3606bb7386e
Erin Beatrice Micaela G. Reyes <89332224+YumenoRetort@users.noreply.github.com>
2024-04-30 20:50:16 +0800

    Add Images to product.php

--- Admin/product.php
+++ Admin/product.php
@@ -1,27 +1,31 @@
-
-<form method="post" action="">
-    Name: <input type="text" name="food_name"><br>
-    Price: <input type="text" name="food_price"><br>
-    Description: <input type="text" name="food_description"><br>
+<form method="post" enctype="multipart/form-data" action="">
+    Name: <input type="text" name="food_name" class="input_fields" required><br>
+    Price: <input type="text" name="food_price" class="input_fields" required><br>
+    Description: <input type="text" name="food_description" class="input_fields" required><br>
+    Image: <input type="file" name="food_image" class="input_fields" required accept="image/png, image/jpg, image/jpeg"><br>
     <input type="submit" value="Submit">
 </form>
-
 
 <?php
 include 'connect.php';
 
-// Assuming you have received product details via POST method
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Extracting product details from the POST data
-    $food_name        = $_POST['food_name'];
-    $food_price       = $_POST['food_price'];
+    $food_name = $_POST['food_name'];
+    $food_price = $_POST['food_price'];
     $food_description = $_POST['food_description'];
+    $food_image = $_FILES['food_image']['name']; 
+    $food_image_temp  = $_FILES['food_image']['tmp_name']; 
+    $upload_dir = '../images/';
 
-    // Inserting product details into the database
-    $insert_query = "INSERT INTO food_products (food_name, food_price, food_description) 
-                     VALUES ('$food_name', '$food_price', '$food_description')";
-    
-    // Running the insert query
+    if (move_uploaded_file($food_image_temp, $upload_dir . $food_image)) {
+        echo "Image uploaded successfully. ";
+    } else {
+        echo "Failed to upload image.";
+    }
+
+    $insert_query = "INSERT INTO food_products (food_name, food_price, food_description, food_image) 
+                     VALUES ('$food_name', '$food_price', '$food_description', '$food_image')";
+
     if(mysqli_query($con, $insert_query)) {
         echo "Food item inserted successfully!";
     } else {
