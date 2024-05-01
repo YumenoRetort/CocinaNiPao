b82d11e0234f858ef038639b826c52526dcf9e54
Erin Beatrice Micaela G. Reyes <89332224+YumenoRetort@users.noreply.github.com>
2024-05-01 16:15:13 +0800

    Create Login Function

--- Database/cocina_ni_pao.sql
+++ Database/cocina_ni_pao.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 30, 2024 at 11:21 AM
+-- Generation Time: May 01, 2024 at 10:14 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -36,6 +36,13 @@ CREATE TABLE `customer` (
   `address` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`customer_id`, `customer_name`, `email`, `password`, `mobile`, `address`) VALUES
+(1, 'Erin Reyes', 'ebmgreyes@mymail.mapua.edu.ph', 'Aliceiw@143', '0918989273', 'BLK 7A LOT 2 JOB STREET JUANA');
+
 -- --------------------------------------------------------
 
 --
@@ -46,8 +53,75 @@ CREATE TABLE `food_products` (
   `food_id` int(10) NOT NULL,
   `food_name` varchar(255) NOT NULL,
   `food_price` int(255) NOT NULL,
-  `food_description` text NOT NULL
+  `food_description` text NOT NULL,
+  `food_image` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `food_products`
+--
+
+INSERT INTO `food_products` (`food_id`, `food_name`, `food_price`, `food_description`, `food_image`) VALUES
+(13, 'Chicken', 123, 'Spicy Chicken', 0x696d616765732e6a7067);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `order_id` int(10) NOT NULL,
+  `customer_id` int(10) NOT NULL,
+  `staff_id` int(10) NOT NULL,
+  `order_status` varchar(255) NOT NULL,
+  `order_recieved` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `payment_id` int(10) NOT NULL,
+  `order_id` int(10) NOT NULL,
+  `total_amount` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shipments`
+--
+
+CREATE TABLE `shipments` (
+  `shipment_id` int(10) NOT NULL,
+  `order_id` int(10) NOT NULL,
+  `delivery_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `staff_id` int(10) NOT NULL,
+  `staff_name` varchar(255) NOT NULL,
+  `staff_email` varchar(255) NOT NULL,
+  `staff_password` varchar(255) NOT NULL,
+  `staff_mobile` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`staff_id`, `staff_name`, `staff_email`, `staff_password`, `staff_mobile`) VALUES
+(3, 'Erin', 'erinreyes1431@gmail.com', 'Aliceiw@143', '0918989273');
 
 --
 -- Indexes for dumped tables
@@ -66,6 +140,32 @@ ALTER TABLE `food_products`
   ADD PRIMARY KEY (`food_id`);
 
 --
+-- Indexes for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `staff_id` (`staff_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`payment_id`);
+
+--
+-- Indexes for table `shipments`
+--
+ALTER TABLE `shipments`
+  ADD PRIMARY KEY (`shipment_id`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`staff_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -73,13 +173,48 @@ ALTER TABLE `food_products`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `customer_id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `customer_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `food_products`
 --
 ALTER TABLE `food_products`
-  MODIFY `food_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `food_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `order_details`
+--
+ALTER TABLE `order_details`
+  MODIFY `order_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `payment_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `shipments`
+--
+ALTER TABLE `shipments`
+  MODIFY `shipment_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `staff_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`),
+  ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`staff_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
