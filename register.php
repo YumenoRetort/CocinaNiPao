bf119e67e9f748cdcca871ee2ac25e727da55564
magnosium <149135204+magnosium@users.noreply.github.com>
2024-05-07 22:49:41 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,24 +1,165 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Cocina ni Pao</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title>Cocina ni Pao | Register</title>
 
-	<link rel = "stylesheet" href="css/style.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        body {
+            background-color: #fab438;
+            margin: 0;
+            padding: 0;
+            font-family: "Poppins", sans-serif;
+        }
+
+        .container-custom {
+            background-color: #efeae4;
+            border-top-left-radius: 50px;
+            border-bottom-left-radius: 50px;
+            padding: 80px;
+            margin-right: -15px;
+        }
+
+        .left-side {
+            padding: 50px;
+            box-sizing: border-box;
+        }
+
+        img {
+            max-width: 25%;
+            height: auto;
+            display: block;
+        }
+
+        form {
+            margin-top: 20px;
+            padding-top: 38px;
+            padding-right: 100px;
+            padding-left: 40px;
+            position: relative;
+        }
+
+        input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            font-family: "Poppins", sans-serif;
+        }
+
+        input[type="text"] {
+            width: 100%;
+            margin-bottom: 10px;
+            padding: 10px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            font-family: "Poppins", sans-serif;
+        }
+
+        input[type="submit"] {
+            background-color: #999b85;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            border-radius: 25px;
+            cursor: pointer;
+            width: 100%;
+            font-family: "Poppins", sans-serif;
+            transition: transform 0.3s ease;
+        }
+
+        input[type="submit"]:hover {
+            transform: scale(1.1);
+            background-color: #999b85
+        }
+
+        h1 {
+            margin-left: 15px;
+            font-size: 75px;
+        }
+
+        .login-btn {
+            color: #f03838;
+            background-color: transparent;
+            border: 2px solid #f03838;
+            border-radius: 25px;
+            width: 100%;
+            font-family: "Poppins", sans-serif;
+            transition: background-color 0.3s ease, color 0.3s ease;
+        }
+
+        .login-btn:hover {
+            background-color: #f03838;
+            color: white; 
+            border-color: transparent;
+        }
+
+        .line {
+            border-bottom: 1px solid black;
+            width: 50%;
+            text-align: center;
+            line-height: 0.1em;
+            margin: 10px 0 20px;
+        }
+
+        .line span {
+            background-color: #efeae4;
+            padding: 0 10px;
+        }
+    </style>
 </head>
 <body>
 
-    <form method="post" action="">
-        Name: <input type="text" name="customer_name"><br>
-        Email: <input type="text" name="email"><br>
-        Password: <input type="password" name="password"><br>
-        Confirm Password: <input type="password" name="confirm_password"><br>
-        Mobile: <input type="text" name="mobile"><br>
-        Address: <input type="text" name="address"><br>
-        <input type="submit" value="Submit">
-    </form>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 left-side">
+                <img src="images/logohome.png" alt="Landscape Image">
+                <br>
+                <h1>Create your <br> account</h1>
+            </div>
+            <div class="col-md-6 d-flex justify-content-center align-items-center">
+                <div class="container-custom">
+                    <form method="post" action="">
+                        <h2>Create Account</h2>
+                        <br>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="customer_name" placeholder="Name">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="email" placeholder="Email">
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <input type="password" class="form-control" name="password" placeholder="Password">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="mobile" placeholder="Mobile">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="address" placeholder="Address">
+                        </div>
+                        <br>
+                        <input type="submit" class="btn btn-primary" value="Sign Up">
+                        <br><br>
+                        <center><div class="line"><span>Or</span></div></center>
+                        <br>
+                        <button type="button" class="btn login-btn" href="login.php">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <?php
     include 'Admin/connect.php';
@@ -47,9 +188,9 @@
     }
     ?>
 
-	<script src="js/script.js"></script>
-	
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
-
-
