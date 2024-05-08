9e6c4c274f4e8f7dad2bd930ef54ebcb93ca4c11
jillwassabi <1jill.sgarcia@gmail.com>
2024-05-09 06:13:24 +0800

    updated about us

--- about.php
+++ about.php
@@ -1,20 +1,94 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>About Page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <style>
 
-	<link rel = "stylesheet" href="css/style.css">
+      .about-us {
+        font-size: 96px;
+        text-align: center;
+        margin-left: 500px;
+        margin-top: 20px;
+		width:500px;
+      }
+
+      .about {
+        color: #474747;
+      }
+
+      .us {
+        color: #FAB438;
+      }
+
+      .AboutTheChef,
+      .AboutTheRestaurant {
+        color: #474747;
+        font-family: 'Poppins', sans-serif;
+        font-weight: bold;
+        font-size: 24px;
+        word-wrap: break-word;
+        text-align: center;
+      }
+
+      .Description1,
+      .Description2 {
+        color: #474747;
+        font-family: 'Poppins', sans-serif;
+        font-weight: normal;
+        font-size: 20px;
+        word-wrap: break-word;
+        text-align: center;
+        margin-top: 10px;
+      }
+
+      body {
+            background-image: url('images/bg.png');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+
+      }
+    </style>
+
 </head>
-<body>
-	 
 
-    <?php 
+<?php 
     include('header.php');
 	?>
 	<script src="js/script.js"></script>
-	
+
+<body>
+
+
+            <div class="col-md-6">
+                <h1 class="about-us"><span class="about">About</span> <span class="us">Us</span></h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="AboutTheChef" style="width: 513px; height: 30px; left: 482px; top: 275px; position: absolute; text-align: center; color: black; font-size: 24px; font-weight: 450; word-wrap: break-word">About the Chef</div>
+  <div class="Description1" style="width: 754px; height: 30px; left: 350px; top: 310px; position: absolute; text-align: justify; color: black; font-size: 12px; font-family: Poppins; font-weight: 350; word-wrap: break-word">
+    Hey there, I’m Paolo, the proud owner of Cocina Lokal. Here at Cocina Lokal, we believe that good food doesn’t have to come with a hefty price tag. We cater to the mid-level market, offering delicious meals that are accessible to everyone.
+    As a chef, my passion lies in creating unique and flavorful dishes that everyone can enjoy. At Cocina Lokal, we specialize in food delivery services, bringing our mouthwatering pastries and savory delights straight to your doorstep.
+    <br><br>
+    Our pastries are where we truly shine. We take classic Filipino recipes and give them an exciting twist, creating what we like to call Filipino Fusion. Take our leche flan, for example. We've added a Mexican twist to this classic Filipino dessert by incorporating cinnamon, giving it a whole new dimension of flavor.
+ 
+  </div>
+
+  <div class="AboutTheRestaurant" style="width: 513px; height: 30px; left: 480px; top: 500px; position: absolute; color: black; font-size: 24px; font-weight: 450; word-wrap: break-word">About the Restaurant</div>
+  <div class="Description2" style="width: 754px; height: 30px; left: 350px; top: 538px; position: absolute; text-align:justify; color: black; font-size: 12px; font-family: Poppins; font-weight: 350; word-wrap: break-word">
+    Welcome to Cocina Lokal, where good food, great attitude, and stunning sunsets come together to create an unforgettable dining experience. Located in Sta. Rosa, Philippines, Cocina Lokal is the brainchild of Chef Paolo, a small business owner with a passion for bringing delicious food to the mid-level market.
+     At Cocina Lokal, we specialize in food delivery services, ensuring that our mouthwatering pastries and savory delights are accessible to everyone. Our menu is a celebration of Filipino Fusion, where classic Filipino recipes are given an exciting twist. From our signature leche flan with a Mexican twist to our savory delights, every bite is a journey of flavor.
+    <br><br>
+    Join us at Cocina Lokal and experience the perfect blend of great food, warm hospitality, and breathtaking sunsets. Please note that we are closed on Sundays and Mondays, but we look forward to serving you the rest of the week!
+  </div>
+
 </body>
 </html>
-
